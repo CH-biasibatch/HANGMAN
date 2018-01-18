d298da51eb9744afd86d159d1db981e22022b2ec
chiara biasi batchelder <chiarabiasibatchel@students.berkeley.net>
2018-01-18 10:43:29 -0800

    hangman updated

--- script.js
+++ script.js
@@ -1,30 +1,86 @@
 var word = "";
 var words = ["grapefruit","bagpipes","dwarves","elephant","galaxy","voodoo","wristwatch","yoked"];
 var guesses = 8;
-var  guessedLetters = [];
+var guessedLetters = [];
+
 
 
 function startGame() {
     word = words[Math.floor(Math.random()* words.length)];
-    document.getElementById("board").innerHTML = printWord();
+    guesses = 8;
+    guessedLetters = [];
+    document.getElementById("messageLOSS").innerHTML = "";
+    document.getElementById("messageWIN").innerHTML = "";
+    printWord();
+    document.getElementById("guesses").innerHTML = "Extra guesses: " + guesses + "<br> Guessed Letters: " + guessedLetters;
+
+    var buttons = document.getElementsByTagName('button');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = false;
+    }
 }
 
 function guessLetter(letter) {
-    letter = letter.toLowerCase();
-    guessedLetters += letter;
-    document.getElementById("board").innerHTML = printWord();
-
+    if(guesses==0) {
+        document.getElementById("messageLOSS").innerHTML = "YOU LOSE TRY AGAIN";
+        return;
+    }else{
+        if(word.indexOf(letter) == -1) {
+            guesses--;
+        }
+    }
+    document.getElementById(letter).disabled = true;
+    guessedLetters.push(letter);
+    printWord();
+    document.getElementById("guesses").innerHTML = "Extra guesses: " + guesses + "<br> Guessed Letters: " + guessedLetters;
 }
 
-
-function printWord(){
+function printWord() {
     var retWord = "";
-    for(var i = 0; i < word.length; i++) {
+    for (var i = 0; i < word.length; i++) {
         if (guessedLetters.indexOf(word[i]) == -1) {
             retWord += "_ ";
         } else {
             retWord += word[i]
         }
     }
+
+    document.getElementById("board").innerHTML = retWord;
+    document.getElementById("imageHangman").innerHTML = "<img src='images/" + getImage() + "'>";
+    if (retWord.indexOf("_") == -1){
+        document.getElementById("messageWIN").innerHTML = "YOU WIN";
+    }
     return retWord;
+}
+
+
+function getImage(){
+    var imageIndex=["Hangman-0.png","Hangman-1.png","Hangman-2.png","Hangman-3.png","Hangman-4.png","Hangman-5.png","Hangman-6.png","Hangman-7.png","Hangman-8.png"];
+    if(guesses==8){
+        return imageIndex[0];
+    }
+    if(guesses==7){
+        return imageIndex[1];
+    }
+    if(guesses==6){
+        return imageIndex[2];
+    }
+    if(guesses==5){
+        return imageIndex[3];
+    }
+    if(guesses==4){
+        return imageIndex[4];
+    }
+    if(guesses==3){
+        return imageIndex[5];
+    }
+    if(guesses==2){
+        return imageIndex[6];
+    }
+    if(guesses==1){
+        return imageIndex[7];
+    }
+    if(guesses==0){
+        return imageIndex[8];
+    }
 }
