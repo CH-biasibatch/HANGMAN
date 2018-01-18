559dcede5e0c185e490f703b16d9de4ab1cd375a
chiara biasi batchelder <chiarabiasibatchel@students.berkeley.net>
2018-01-18 10:56:44 -0800

    hangman update 2.0

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var word = "";
-var words = ["grapefruit","bagpipes","dwarves","elephant","galaxy","voodoo","wristwatch","yoked","abruptly","daiquiri","unzip","quixotic","phlegm","pajama","yachtsman","strengths"];
+var words = ["grapefruit","bagpipes","elephant","galaxy","voodoo","wristwatch","yoked","abruptly","daiquiri","unzip","quixotic","phlegm","pajama","yachtsman","strengths"];
 var guesses = 8;
 var guessedLetters = [];
 
